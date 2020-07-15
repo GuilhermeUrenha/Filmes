40488032782f34ad19095a240b8e8dd80becdc21
GuilhermeUrenha <guilherme.urenha@gmail.com>
2020-07-15 04:37:10 -0300

    Finalização da tela do Filme; Criação da busca;

--- app/src/main/java/com/example/filmes/Filme.java
+++ app/src/main/java/com/example/filmes/Filme.java
@@ -2,13 +2,19 @@ package com.example.filmes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Filme extends AppCompatActivity {
 
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filme);
+
+        intent = this.getIntent();
+        System.out.println(intent.getStringExtra("test"));
     }
 }
--- app/src/main/java/com/example/filmes/MainActivity.java
+++ app/src/main/java/com/example/filmes/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.filmes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    Intent intent;
+    public void Search(View view){
+        intent = new Intent(this, Filme.class);
+        intent.putExtra("test","it was a mistake");
+        this.startActivity(intent);
     }
 }
